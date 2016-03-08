2d13f314b7019952b8ac8b02a2bb4e50cb4694e8
Regis <boudinot.regis@yahoo.com>
2016-03-08 15:56:19 -0700

    intialize_life is becoming more powerful

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,15 @@ fn main() {
                                  mag: Cell::new(0)
                               };
 
-    initialize_life(&mut block);
+    initialize_life(&mut block, 0, 0, 0, 0, 0);
 }
 
-fn initialize_life(b: &mut LifeBlock) -> &LifeBlock {
-    let origin : [i32;2] = [0, 0];
+fn initialize_life(b: &mut LifeBlock, x: i32, y: i32, z: i32, c: i32, m: i32) -> &LifeBlock {
+    let origin : [i32;2] = [x, y];
     b.x_y = vec![origin];
-    b.z = vec![0];
-    b.charge.set(0);
-    b.mag.set(0);
+    b.z = vec![z];
+    b.charge.set(c);
+    b.mag.set(m);
     b
 }
 
@@ -36,9 +36,26 @@ fn it_can_begin() {
                                  mag: Cell::new(0)
                               };
 
-    initialize_life(&mut block);
+    initialize_life(&mut block, 0, 0, 0, 0, 0);
 
     assert_eq!(block.x_y, [[0, 0]]);
+    assert_eq!(block.z, [0]);
+    assert_eq!(block.charge.get(), 0);
+    assert_eq!(block.mag.get(), 0);
+}
+
+#[test]
+fn it_can_make_another_life_block_in_a_new_position() {
+    let mut block = LifeBlock {
+                                 x_y: Vec::new(),
+                                 z: Vec::new(),
+                                 charge: Cell::new(0),
+                                 mag: Cell::new(0)
+                              };
+
+    initialize_life(&mut block, 0, 1, 0, 0, 0);
+
+    assert_eq!(block.x_y, [[0, 1]]);
     assert_eq!(block.z, [0]);
     assert_eq!(block.charge.get(), 0);
     assert_eq!(block.mag.get(), 0);
