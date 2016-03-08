82c92f942d76984a4687eb8b28a4ecb0829b1b7c
Regis <boudinot.regis@yahoo.com>
2016-03-08 08:47:26 -0700

    better setup - more logical data flow

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
 struct LifeBlock {
-    x_and_y: Vec<[i32;2]>,
+    x_y: Vec<[i32;2]>,
     z:       Vec<i32>,
 }
 
 fn main() {
-    let mut structure = LifeBlock {x_and_y: Vec::new(), z: Vec::new()};
+    let mut block = LifeBlock {x_y: Vec::new(), z: Vec::new()};
 
-    let std : [i32;2] = [0, 0];
-
-    structure.x_and_y = vec![std];
-    structure.z       = vec![0];
-
-    identify_structure(&mut structure.x_and_y, &mut structure.z);
+    initialize_life(&mut block);
 }
 
-fn identify_structure(x_and_y: &mut Vec<[i32;2]>, z: &mut Vec<i32>) {
-    println!("{:?}", x_and_y);
-    println!("{:?}", z);
+fn initialize_life(b: &mut LifeBlock) -> &LifeBlock {
+    let std : [i32;2] = [0, 0];
+    b.x_y = vec![std];
+    b.z = vec![0];
+    b
 }
 
 #[test]
 fn it_can_begin() {
-    let mut structure = LifeBlock {x_and_y: Vec::new(), z: Vec::new()};
+    let mut block = LifeBlock {x_y: Vec::new(), z: Vec::new()};
 
-    let std : [i32;2] = [0, 0];
+    initialize_life(&mut block);
 
-    structure.x_and_y = vec![std];
-
-    assert_eq!(structure.x_and_y, [[0, 0]]);
-    assert_eq!(structure.z, []);
+    assert_eq!(block.x_y, [[0, 0]]);
+    assert_eq!(block.z, [0]);
 }
