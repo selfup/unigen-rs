a9f3a69c24676431f94008644b09efca5dfd05d9
Regis <boudinot.regis@yahoo.com>
2016-03-08 01:13:49 -0700

    beg-one - just the start

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+struct LifeBlock {
+    x_and_y: Vec<[i32;2]>,
+    z:       Vec<i32>,
+}
+
 fn main() {
-    let message = "alive";
-    println!("{:?}", message);
+    let mut structure = LifeBlock {x_and_y: Vec::new(), z: Vec::new()};
+
+    let std : [i32;2] = [0, 0];
+
+    structure.x_and_y = vec![std];
+    structure.z       = vec![0];
+
+    identify_structure(&mut structure.x_and_y, &mut structure.z);
+}
+
+fn identify_structure(x_and_y: &mut Vec<[i32;2]>, z: &mut Vec<i32>) {
+    println!("{:?}", x_and_y);
+    println!("{:?}", z);
 }
