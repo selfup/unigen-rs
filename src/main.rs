4ce78573a7b8fef80b772e4b1a2c67a0a67007f4
Regis <boudinot.regis@yahoo.com>
2016-03-08 19:42:14 -0700

    better memory management

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 struct LifeBlock {
     x_y:    (i32, i32),
-    z:      Vec<i32>,
+    z:      i32,
     charge: i32,
     mag:    i32,
 }
@@ -18,7 +18,7 @@ fn initialize_life(limit: i32, container: &mut Vec<LifeBlock>) {
     for v in 0..limit + 1 {
         for w in 0..limit + 1 {
             for q in 0..limit + 1 {
-                container.push(LifeBlock { x_y: (v, w), z: vec![q], charge: 0, mag: 0 })
+                container.push(LifeBlock { x_y: (v, w), z: q, charge: 0, mag: 0 })
             }
         }
     }
@@ -33,29 +33,29 @@ fn it_can_begin() {
     assert_eq!(universe.len(), 216);
 
     assert_eq!(universe[0].x_y, (0, 0));
-    assert_eq!(universe[0].z, [0]);
+    assert_eq!(universe[0].z, 0);
 
     assert_eq!(universe[1].x_y, (0, 0));
-    assert_eq!(universe[1].z, [1]);
+    assert_eq!(universe[1].z, 1);
 
     assert_eq!(universe[2].x_y, (0, 0));
-    assert_eq!(universe[2].z, [2]);
+    assert_eq!(universe[2].z, 2);
 
     assert_eq!(universe[6].x_y, (0, 1));
-    assert_eq!(universe[6].z, [0]);
+    assert_eq!(universe[6].z, 0);
 
     assert_eq!(universe[7].x_y, (0, 1));
-    assert_eq!(universe[7].z, [1]);
+    assert_eq!(universe[7].z, 1);
 
     assert_eq!(universe[8].x_y, (0, 1));
-    assert_eq!(universe[8].z, [2]);
+    assert_eq!(universe[8].z, 2);
 
     assert_eq!(universe[12].x_y, (0, 2));
-    assert_eq!(universe[12].z, [0]);
+    assert_eq!(universe[12].z, 0);
 
     assert_eq!(universe[13].x_y, (0, 2));
-    assert_eq!(universe[13].z, [1]);
+    assert_eq!(universe[13].z, 1);
 
     assert_eq!(universe[20].x_y, (0, 3));
-    assert_eq!(universe[20].z, [2]);
+    assert_eq!(universe[20].z, 2);
 }
