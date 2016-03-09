cbead0f4a9cdd45ca34bf96b74b7863d086c6e67
Regis <boudinot.regis@yahoo.com>
2016-03-08 17:29:34 -0700

    test passing

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,59 @@
-use std::cell::Cell;
-
 struct LifeBlock {
-    x_y: Vec<[i32;2]>,
+    x_y: (i32, i32),
     z:       Vec<i32>,
-    charge: Cell<i32>,
-    mag:    Cell<i32>,
+    charge: i32,
+    mag:    i32,
 }
 
 fn main() {
+
     let mut block = LifeBlock {
-                                 x_y: Vec::new(),
+                                 x_y: (0, 0),
                                  z: Vec::new(),
-                                 charge: Cell::new(0),
-                                 mag: Cell::new(0)
+                                 charge: 0,
+                                 mag: 0
                               };
 
-    initialize_life(&mut block, 0, 0, 0, 0, 0);
 }
 
 fn initialize_life(b: &mut LifeBlock, x: i32, y: i32, z: i32, c: i32, m: i32) -> &LifeBlock {
-    let origin : [i32;2] = [x, y];
-    b.x_y = vec![origin];
+    b.x_y = (x, y);
     b.z = vec![z];
-    b.charge.set(c);
-    b.mag.set(m);
+    b.charge = c;
+    b.mag = m;
     b
 }
 
 #[test]
 fn it_can_begin() {
     let mut block = LifeBlock {
-                                 x_y: Vec::new(),
+                                 x_y: (0, 0),
                                  z: Vec::new(),
-                                 charge: Cell::new(0),
-                                 mag: Cell::new(0)
+                                 charge: 0,
+                                 mag: 0
                               };
 
     initialize_life(&mut block, 0, 0, 0, 0, 0);
 
-    assert_eq!(block.x_y, [[0, 0]]);
+    assert_eq!(block.x_y, (0, 0));
     assert_eq!(block.z, [0]);
-    assert_eq!(block.charge.get(), 0);
-    assert_eq!(block.mag.get(), 0);
+    assert_eq!(block.charge, 0);
+    assert_eq!(block.mag, 0);
 }
 
 #[test]
 fn it_can_make_another_life_block_in_a_new_position() {
     let mut block = LifeBlock {
-                                 x_y: Vec::new(),
+                                 x_y: (0, 0),
                                  z: Vec::new(),
-                                 charge: Cell::new(0),
-                                 mag: Cell::new(0)
+                                 charge: 0,
+                                 mag: 0
                               };
 
     initialize_life(&mut block, 0, 1, 0, 0, 0);
 
-    assert_eq!(block.x_y, [[0, 1]]);
+    assert_eq!(block.x_y, (0, 1));
     assert_eq!(block.z, [0]);
-    assert_eq!(block.charge.get(), 0);
-    assert_eq!(block.mag.get(), 0);
+    assert_eq!(block.charge, 0);
+    assert_eq!(block.mag, 0);
 }
