b7a630f8322db10647b1392d0fb25be5bd63c901
selfup <boudinot.regis@yahoo.com>
2020-11-11 15:40:48 -0700

    can render atoms as cubes with color based on charge

--- src/builder.rs
+++ src/builder.rs
@@ -3,97 +3,103 @@ use rayon::prelude::*;
 
 mod core;
 
-pub fn initialize_universe(parsed_size: u32, uni: &mut Vec<core::Block>) -> Vec<core::Block> {
-    let mut id: u32 = 0;
+// Blocks builds blocks..
+pub struct Blocks {}
 
-    for x in 0..parsed_size {
-        for y in 0..parsed_size {
-            for z in 0..parsed_size {                
-                let (electrons, protons, neutrons): (u32, u32, u32) = (0, 0, 0);
-
-                uni.push(core::Block {
-                    id,
-                    x,
-                    y,
-                    z,
-                    charge: 0,
-                    atom: core::Atom {
-                       electrons,
-                        nucleus: core::Nucleus {
-                            protons,
-                            neutrons,
+impl Blocks {
+    pub fn initialize_universe(parsed_size: u32, uni: &mut Vec<core::Block>) -> Vec<core::Block> {
+        let mut id: u32 = 0;
+    
+        for x in 0..parsed_size {
+            for y in 0..parsed_size {
+                for z in 0..parsed_size {                
+                    let (electrons, protons, neutrons): (u32, u32, u32) = (0, 0, 0);
+    
+                    uni.push(core::Block {
+                        id,
+                        x,
+                        y,
+                        z,
+                        charge: 0,
+                        atom: core::Atom {
+                           electrons,
+                            nucleus: core::Nucleus {
+                                protons,
+                                neutrons,
+                            },
                         },
-                    },
-                });
-
-                id += 1;
+                    });
+    
+                    id += 1;
+                }
+            }
+        }
+    
+        println!("Threads: {}", rayon::current_num_threads());
+    
+        uni.clone()
+    }
+    
+    pub fn particles(universe: &mut Vec<core::Block>, neutron: &mut [u32; 1], proton: &mut [u32; 1], electron: &mut [u32; 1]) {
+        neutron[0] = universe.par_iter().map(|i| i.atom.nucleus.neutrons).sum();
+        proton[0] = universe.par_iter().map(|i| i.atom.nucleus.protons).sum();
+        electron[0] = universe.par_iter().map(|i| i.atom.electrons).sum();
+    }
+    
+    pub fn charge_of_field(proton: &mut [u32; 1], electron: &mut [u32; 1], u: u32) {
+        let size: u32 = u * u * u;
+        let cast_size: u32 = size as u32;
+        
+        if proton[0] == cast_size && electron[0] == cast_size {
+            println!("Field is Netural");
+        } else if (proton[0] > cast_size) && (electron[0] < proton[0]) {
+            println!("Field is Cationic");
+        } else {
+            println!("Field is Anionic");
+        }
+    }
+    
+    pub fn atom_charge(universe: &mut Vec<core::Block>) {
+        for block in universe {
+            if block.atom.nucleus.protons == block.atom.electrons {
+                block.charge = 0;
+            } else if block.atom.nucleus.protons > block.atom.electrons {
+                block.charge = 1;
+            } else {
+                block.charge = -1;
             }
         }
     }
-
-    println!("Threads: {}", rayon::current_num_threads());
-
-    uni.clone()
-}
-
-pub fn particles(universe: &mut Vec<core::Block>, neutron: &mut [u32; 1], proton: &mut [u32; 1], electron: &mut [u32; 1]) {
-    neutron[0] = universe.par_iter().map(|i| i.atom.nucleus.neutrons).sum();
-    proton[0] = universe.par_iter().map(|i| i.atom.nucleus.protons).sum();
-    electron[0] = universe.par_iter().map(|i| i.atom.electrons).sum();
-}
-
-pub fn charge_of_field(proton: &mut [u32; 1], electron: &mut [u32; 1], u: u32) {
-    let size: u32 = u * u * u;
-    let cast_size: u32 = size as u32;
     
-    if proton[0] == cast_size && electron[0] == cast_size {
-        println!("Field is Netural");
-    } else if (proton[0] > cast_size) && (electron[0] < proton[0]) {
-        println!("Field is Cationic");
-    } else {
-        println!("Field is Anionic");
-    }
-}
-
-pub fn atom_charge(universe: &mut Vec<core::Block>) {
-    for block in universe {
-        if block.atom.nucleus.protons == block.atom.electrons {
-            block.charge = 0;
-        } else if block.atom.nucleus.protons > block.atom.electrons {
-            block.charge = 1;
-        } else {
-            block.charge = -1;
-        }
-    }
-}
-
-pub fn tick(parsed_size: u32, universe: &mut Vec<core::Block>) -> Vec<core::Block> {
-    let mut uni_copy: Vec<core::Block> = universe.clone();
+    pub fn tick(parsed_size: u32, universe: &mut Vec<core::Block>) -> Vec<core::Block> {
+        let mut uni_copy: Vec<core::Block> = universe.clone();
+        
+        let chunk_size: usize = (parsed_size) as usize;
     
-    let chunk_size: usize = (parsed_size) as usize;
-
-    uni_copy.par_chunks_mut(chunk_size).for_each_init(|| rand::thread_rng(), |rng, blocks| {
-        for block in blocks {
-            let (electrons, protons, neutrons): (u32, u32, u32) = (
-                rng.gen_range(0, 118),
-                rng.gen_range(0, 118),
-                rng.gen_range(0, 118),
-            );
-
-            block.atom.electrons = electrons;
-            block.atom.nucleus.protons = protons;
-            block.atom.nucleus.neutrons = neutrons;
-        }
-    });
-
-    uni_copy
+        uni_copy.par_chunks_mut(chunk_size).for_each_init(|| rand::thread_rng(), |rng, blocks| {
+            for block in blocks {
+                let (electrons, protons, neutrons): (u32, u32, u32) = (
+                    rng.gen_range(0, 118),
+                    rng.gen_range(0, 118),
+                    rng.gen_range(0, 118),
+                );
+    
+                block.atom.electrons = electrons;
+                block.atom.nucleus.protons = protons;
+                block.atom.nucleus.neutrons = neutrons;
+            }
+        });
+    
+        uni_copy
+    }    
 }
+
 
 #[test]
 fn it_can_begin() {
     let mut universe: Vec<core::Block> = vec![];
     
-    initialize_universe(5, &mut universe);
+    Blocks::initialize_universe(5, &mut universe);
 
     assert_eq!(universe.len(), 125);
     
@@ -117,9 +123,9 @@ fn it_can_infer_the_charge_of_an_atom() {
     
     let mut rando = "";
     
-    let mut generated_universe: Vec<core::Block> = initialize_universe(5, &mut universe);
-    particles(&mut generated_universe, &mut neturon, &mut proton, &mut electron);
-    atom_charge(&mut generated_universe);
+    let mut generated_universe: Vec<core::Block> = Blocks::initialize_universe(5, &mut universe);
+    Blocks::particles(&mut generated_universe, &mut neturon, &mut proton, &mut electron);
+    Blocks::atom_charge(&mut generated_universe);
     
     assert_eq!(generated_universe.len(), 125);
 
@@ -147,8 +153,8 @@ fn it_can_sense_the_field() {
     let mut proton: [u32; 1] = [0];
     let mut electron: [u32; 1] = [0];
 
-    universe = initialize_universe(2, &mut universe);
-    particles(&mut universe, &mut neturon, &mut proton, &mut electron);
+    universe = Blocks::initialize_universe(2, &mut universe);
+    Blocks::particles(&mut universe, &mut neturon, &mut proton, &mut electron);
 
     assert_eq!(universe.len(), 8);
     assert_eq!(neturon.len(), 1);
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,31 @@ use std::env;
 use bevy::prelude::*;
 
 mod builder;
+use builder::Blocks;
 
 fn main() {
+    App::build()
+        .add_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // // add entities to the world
+    // commands
+    //     // plane
+    //     .spawn(PbrComponents {
+    //         mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+    //         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    //         ..Default::default()
+    //     });
+
     let mut size = String::new();
     let args: Vec<String> = env::args().collect();
 
@@ -22,45 +45,46 @@ fn main() {
     let mut proton: [u32; 1] = [0];
     let mut electron: [u32; 1] = [0];
 
-    let mut generated_universe = builder::initialize_universe(parsed_size, &mut universe);
+    let mut generated_universe = Blocks::initialize_universe(parsed_size, &mut universe);
 
-    generated_universe = builder::tick(parsed_size, &mut generated_universe);
-    builder::particles(&mut generated_universe, &mut neturon, &mut proton, &mut electron);
+    generated_universe = Blocks::tick(parsed_size, &mut generated_universe);
+    Blocks::particles(&mut generated_universe, &mut neturon, &mut proton, &mut electron);
 
     println!("Snapshot..\n\n{:?}\n", &generated_universe[0]);
     println!("Universe built!\nChecking the charge..");
 
-    builder::charge_of_field(&mut proton, &mut electron, parsed_size);
-    builder::atom_charge(&mut generated_universe);
+    Blocks::charge_of_field(&mut proton, &mut electron, parsed_size);
+    Blocks::atom_charge(&mut generated_universe);
 
     println!("Size of Universe: {:?}", generated_universe.len());
 
-    App::build()
-        .add_startup_system(setup.system()) // <--
-        .add_startup_stage("game_setup") // <--
-        .add_startup_system_to_stage("game_setup", spawn_atom.system())
-        .add_plugins(DefaultPlugins)
-        .run();
-}
+    for block in generated_universe {
+        let y = block.y as f32;
+        let x = block.x as f32;
+        let z = block.z as f32;
 
-struct Atom;
-struct Materials {
-    head_material: Handle<ColorMaterial>,
-}
+        let r = block.charge as f32;
 
-fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
-    commands.spawn(Camera2dComponents::default());
-    commands.insert_resource(Materials {
-        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
-    });
-}
+        commands
+        // cube
+            .spawn(PbrComponents {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.2 })),
+                material: materials.add(Color::rgb(r, 0.7, 0.6).into()),
+                transform: Transform::from_translation(Vec3::new(x, y, z)),
+                ..Default::default()
+            });
+    }
 
-fn spawn_atom(mut commands: Commands, materials: Res<Materials>) {
     commands
-        .spawn(SpriteComponents {
-            material: materials.head_material.clone(),
-            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+        // light
+        .spawn(LightComponents {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
         })
-        .with(Atom);
+        // camera
+        .spawn(Camera3dComponents {
+            transform: Transform::from_translation(Vec3::new(-40.0, 30.0, 30.0))
+                .looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        });
 }
