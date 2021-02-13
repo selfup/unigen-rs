838962fb77504ce1c76d4dd5407ca71917a6f1ef
selfup <boudinot.regis@yahoo.com>
2021-02-13 07:19:56 -0700

    add failsafe to script prior to running unigen

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -24,6 +24,14 @@ Running with given size now.
     '
 fi
 
+if [[ $1 == '' ]]
+then
+    echo 'Please pass a number to the script like so: ./scripts/generate.sh 30'
+    echo '$1 arg not given - no size for generator..'
+    echo 'Aborting!'
+    exit 1
+fi
+
 time cargo run -q --release $1
 
 cd ../../
