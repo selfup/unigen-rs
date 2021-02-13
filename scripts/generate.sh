a658bd1c68605157084756cae1a3ddca8872ef12
selfup <boudinot.regis@yahoo.com>
2021-02-13 07:13:22 -0700

    add option to warmup cache prior to first big run

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 cd crates/unigen
 
 cargo build -q --release
+
+if [[ $2 == 'warmup' ]]
+then
+    echo '
+---
+Warming up!
+---
+    '
+
+    time cargo run -q --release 30
+    
+    echo '
+---
+Warmup done!
+Running with given size now.
+---
+    '
+fi
 
 time cargo run -q --release $1
 
